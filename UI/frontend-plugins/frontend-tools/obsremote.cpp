4fe5b4dd090caaaacffc1ddf9d6a60ebc31c1cae
Shaolin <admshao@gmail.com>
2017-02-12 05:07:15 -0200

    Basic support for the OBS Remote Thread
    Signed-off-by: Shaolin <admshao@gmail.com>

--- UI/frontend-plugins/frontend-tools/obsremote.cpp
+++ UI/frontend-plugins/frontend-tools/obsremote.cpp
@@ -8,57 +8,223 @@
 #include <QObject>
 #include "obsremote.hpp"
 
+#include <condition_variable>
+#include <chrono>
+#include <string>
+#include <thread>
+#include <mutex>
+
+#define do_log(type, format, ...) blog(type, "[OBSRemote] " format, \
+                ##__VA_ARGS__)
+
+#define error(format, ...) do_log(LOG_ERROR, format, ##__VA_ARGS__)
+#define debug(format, ...) do_log(LOG_DEBUG, format, ##__VA_ARGS__)
+
 using namespace std;
 
-OBSRemote *remote;
+#define DEFAULT_PORT 4444
+#define DEFAULT_INTERVAL 1000
+
+struct OBSRemoteData
+{
+	thread th;
+	condition_variable cv;
+	mutex m;
+
+	bool enabled = false;
+	int interval = DEFAULT_INTERVAL;
+	int port = DEFAULT_PORT;
+	string password;
+
+	void Loop();
+
+	void Start();
+
+	void Stop();
+
+	inline ~OBSRemoteData()
+	{
+		Stop();
+	}
+};
+
+static OBSRemoteData *obsremote = nullptr;
+static OBSRemote *obsr = nullptr;
 
 OBSRemote::OBSRemote(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui_OBSRemote)
+	QDialog(parent),
+	ui(new Ui_OBSRemote)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+
+	ui->edit_port->setValidator(new QIntValidator(1000, 65535, this));
+
+	QObject::connect(ui->enabled, SIGNAL(clicked()), this,
+	                 SLOT(EnableClicked()));
 }
 
-void OBSRemote::ShowHideDialog()
+void OBSRemoteData::Loop()
 {
-    if (!isVisible()) {
-        setVisible(true);
-        QTimer::singleShot(250, this, SLOT(show()));
-    } else {
-        setVisible(false);
-        QTimer::singleShot(250, this, SLOT(hide()));
-    }
+	debug("Loop");
+	chrono::duration<long long, milli> duration =
+		chrono::milliseconds(interval);
+
+	for (;;) {
+		unique_lock<mutex> lock(m);
+
+		cv.wait_for(lock, duration);
+		if (!obsremote->enabled) {
+			break;
+		}
+
+		duration = chrono::milliseconds(interval);
+
+		debug("Teste");
+	}
 }
 
-void OBSRemote::closeEvent(QCloseEvent*)
+void OBSRemoteData::Start()
 {
+	debug("Start");
+	if (!obsremote->th.joinable())
+	{
+		debug("Joinable");
+		obsremote->th = thread([] () {obsremote->Loop();});
+	}
+}
 
+void OBSRemoteData::Stop()
+{
+	debug("Stop");
+	if (th.joinable()) {
+		debug("Joinable");
+		{
+			lock_guard<mutex> lock(m);
+			enabled = false;
+		}
+		cv.notify_one();
+		th.join();
+	}
+}
+
+void OBSRemote::EnableClicked()
+{
+	if (ui->enabled->isChecked())
+	{
+		obsremote->enabled = true;
+		obsremote->Start();
+		ui->edit_password->setDisabled(true);
+		ui->edit_port->setDisabled(true);
+	} else
+	{
+		obsremote->Stop();
+		ui->edit_password->setDisabled(false);
+		ui->edit_port->setDisabled(false);
+	}
+}
+
+static void SaveOBSRemote(obs_data_t *save_data, bool saving, void *)
+{
+	if (saving)
+	{
+		lock_guard<mutex> lock(obsremote->m);
+		obs_data_t *obj = obs_data_create();
+
+		obs_data_set_bool(obj, "enabled",
+		                  obsr->ui->enabled->isChecked());
+		int port = obsr->ui->edit_port->text().toInt();
+		if (port > 0)
+		{
+			obs_data_set_int(obj, "port", port);
+		} else
+		{
+			obs_data_set_int(obj, "port", DEFAULT_PORT);
+		}
+
+		obs_data_set_string(obj, "password",
+		                    obsr->ui->edit_password->text()
+			                    .toStdString().c_str());
+
+		obs_data_set_obj(save_data, "obsremote", obj);
+		obs_data_release(obj);
+	} else
+	{
+		obsremote->Stop();
+
+		obsremote->m.lock();
+
+		obs_data_t *obj = obs_data_get_obj(save_data, "obsremote");
+
+		if (!obj)
+			obj = obs_data_create();
+
+		obs_data_set_default_int(obj, "port", DEFAULT_PORT);
+
+		obsremote->enabled = obs_data_get_bool(obj, "enabled");
+		obsr->ui->enabled->setChecked(obsremote->enabled);
+		obsr->EnableClicked();
+		obsremote->port = (int) obs_data_get_int(obj, "port");
+		obsr->ui->edit_port->setText(QString::number(obsremote->port));
+		obsremote->password = obs_data_get_string(obj, "password");
+		obsr->ui->edit_password->setText(obsremote->password.c_str());
+
+		obs_data_release(obj);
+		obsremote->m.unlock();
+
+		if (obsremote->enabled)
+			obsremote->Start();
+	}
+}
+
+void OBSRemote::closeEvent(QCloseEvent *)
+{
+	debug("Window Close");
+	obs_frontend_save();
 }
 
 extern "C" void EndOBSRemote()
 {
+	debug("End");
+	delete obsremote;
+	obsremote = nullptr;
+	delete obsr;
+	obsr = nullptr;
+}
+
+static void OBSEvent(enum obs_frontend_event event, void *)
+{
+	debug("Event");
+	if (event == OBS_FRONTEND_EVENT_EXIT)
+	{
+		debug("Exit");
+		EndOBSRemote();
+	}
 }
 
 extern "C" void InitOBSRemote()
 {
-    QAction *action = (QAction*)obs_frontend_add_tools_menu_qaction(
-            obs_module_text("OBSRemote"));
 
-    obs_frontend_push_ui_translation(obs_module_get_string);
+	debug("Init");
+	QAction *action = (QAction *) obs_frontend_add_tools_menu_qaction(
+		obs_module_text("OBSRemote"));
 
-    QMainWindow *window = (QMainWindow*)obs_frontend_get_main_window();
+	obs_frontend_push_ui_translation(obs_module_get_string);
 
-    remote = new OBSRemote(window);
+	QWidget *window = (QWidget *) obs_frontend_get_main_window();
 
-    auto cb = [] ()
-    {
-        remote->ShowHideDialog();
-    };
+	obsremote = new OBSRemoteData;
+	obsr = new OBSRemote(window);
 
-    obs_frontend_pop_ui_translation();
+	auto cb = []()
+	{
+		debug("OBSRemote Window Exec");
+		obsr->exec();
+	};
 
-    //obs_frontend_add_save_callback(SaveOutputTimer, nullptr);
-    //obs_frontend_add_event_callback(OBSEvent, nullptr);
+	obs_frontend_pop_ui_translation();
 
-    action->connect(action, &QAction::triggered, cb);
+	obs_frontend_add_save_callback(SaveOBSRemote, nullptr);
+	obs_frontend_add_event_callback(OBSEvent, nullptr);
+
+	action->connect(action, &QAction::triggered, cb);
 }
--- UI/frontend-plugins/frontend-tools/obsremote.hpp
+++ UI/frontend-plugins/frontend-tools/obsremote.hpp
@@ -5,19 +5,16 @@
 
 #include "ui_obsremote.h"
 
-class QCloseEvent;
-
 class OBSRemote : public QDialog
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
-    std::unique_ptr<Ui_OBSRemote> ui;
-    OBSRemote(QWidget *parent);
+	std::unique_ptr<Ui_OBSRemote> ui;
+	OBSRemote(QWidget *parent);
 
-    void closeEvent(QCloseEvent *event) override;
+	void closeEvent(QCloseEvent *event) override;
 
 public slots:
-    void ShowHideDialog();
-
+	void EnableClicked();
 };
