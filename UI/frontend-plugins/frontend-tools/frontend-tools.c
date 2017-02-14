518d89d6905d3afaea20ecbde5b3d79acb521eca
Shaolin <admshao@gmail.com>
2017-02-14 01:22:06 -0200

    Initial version of libwebsockets for OBS Studio. Still missing: SSL support and tweak CMakeList for OBS build.

--- UI/frontend-plugins/frontend-tools/frontend-tools.c
+++ UI/frontend-plugins/frontend-tools/frontend-tools.c
@@ -1,5 +1,6 @@
 #include <obs-module.h>
 #include "frontend-tools-config.h"
+#include <libwebsockets.h>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("frontend-tools", "en-US")
@@ -19,6 +20,19 @@ void FreeOutputTimer();
 
 bool obs_module_load(void)
 {
+	struct lws_context *context;
+	struct lws_context_creation_info info;
+	memset(&info, 0, sizeof info);
+	info.port = 4444;
+	context = lws_create_context(&info);
+	if (context == NULL) {
+		lwsl_err("libwebsocket init failed\n");
+		return -1;
+	}
+
+	lws_context_destroy(context);
+
+	lwsl_notice("libwebsockets-test-server exited cleanly\n");
 #if defined(_WIN32) || defined(__APPLE__)
 	InitSceneSwitcher();
 #endif
